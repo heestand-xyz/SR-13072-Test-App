eb7a0fc4ec005ef77470a7f1027e4d6d8669653a
Hexagons <anton.heestand@gmail.com>
2020-06-25 15:37:33 +0200

    Initial Commit

---
+++ SR-13072 Test App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SR-13072 Test App
+//
+//  Created by Cappuccino on 2020-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SR-13072 Test App/SR_13072_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SR_13072_Test_AppApp.swift
+//  SR-13072 Test App
+//
+//  Created by Cappuccino on 2020-06-25.
+//
+
+import SwiftUI
+
+@main
+struct SR_13072_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
